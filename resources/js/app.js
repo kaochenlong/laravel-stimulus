bd4638a1a7a7393164a807b24e01eb9def18ee0a
eddie <eddie@5xruby.tw>
2021-09-25 02:26:07 +0800

    install stimulus

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-require('./bootstrap');
+require("./bootstrap")
+
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
---
+++ resources/views/shop.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts/application')
+
+@section('body')
+<h1>Hello World</h1>
+<div data-controller="hello">
+    123
+</div>
+@endsection
